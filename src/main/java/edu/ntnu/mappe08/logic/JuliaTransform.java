5ffa56a71fe749d6edc69e873ed39134e0c09fa8
matrHS <matthew.r.hunt@gmail.com>
2024-02-09 12:22:31 +0100

    Created JuliaTransform and tests

---
+++ src/main/java/edu/ntnu/mappe08/logic/JuliaTransform.java
@@ -0,0 +1,52 @@
+package edu.ntnu.mappe08.logic;
+
+import edu.ntnu.mappe08.entity.Complex;
+import edu.ntnu.mappe08.entity.Vector2D;
+
+/**
+ * Represents a 2D transformation using the Julia transformation.
+ */
+public class JuliaTransform implements Transform2D {
+  private Complex pointC;
+  private int sign;
+
+  /**
+   * Constructor for a Julia transformation.
+   * 
+   * @param point Complex number to use for transformation.
+   * @param sign +1 or -1 for negative or positive transformation.
+   */
+  public JuliaTransform(Complex point, int sign) {
+    this.pointC = point;
+    this.sign = sign;
+  }
+
+  /**
+   * Transforms a 2D vector using the Julia transformation.
+   * 
+   * @param point Complex number to use for transformation.
+   * @return The transformed complex number.
+   */
+  @Override
+  public Vector2D transform(Vector2D point) {
+    if (point == null) {
+      throw new NullPointerException("Passed vector is null");
+    }
+    
+    if (!(point instanceof Complex)) {
+      throw new IllegalArgumentException("Passed vector is not a complex number");
+    }
+    
+    Complex complexPoint = (Complex) point;
+    double realPartResult = pointC.getRealPart() - complexPoint.getRealPart();
+    double imaginaryPartResult = pointC.getImaginaryPart() - complexPoint.getImaginaryPart();
+    
+    Complex complexResult = new Complex(realPartResult, imaginaryPartResult);
+    Complex complexSquare = complexResult.sqrt();
+    
+    Vector2D juliaResult = new Complex(complexSquare.getRealPart() * sign, 
+        complexSquare.getImaginaryPart() * sign);
+    
+    return juliaResult;
+  }
+}
---
+++ src/test/java/edu/ntnu/mappe08/logic/JuliaTransformTest.java
@@ -0,0 +1,70 @@
+package edu.ntnu.mappe08.logic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.ntnu.mappe08.entity.Complex;
+import edu.ntnu.mappe08.entity.Vector2D;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class JuliaTransformTest {
+  Complex testComplex;
+  int sign;
+  JuliaTransform testTransform;
+  
+  /**
+   * Initializes the test values for each test.
+   */
+  @BeforeEach
+  public void initializeTestValues() {
+    this.testComplex = new Complex(11, 46);
+  }
+  
+  /**
+   * Positive test:
+   * Test Julia transform with positive sign.
+   */
+  @Test
+  public void testValidPosSignTransform() {
+    sign = 1;
+    testTransform = new JuliaTransform(testComplex,sign);
+    Complex testPointZ = new Complex(2, 6);
+    
+    Complex result = (Complex) testTransform.transform(testPointZ);
+    
+    assertEquals(5, result.getRealPart());
+    assertEquals(4, result.getImaginaryPart());
+  }
+
+  /**
+   * Positive test:
+   * Test Julia transform with negative sign.
+   */
+  @Test
+  public void testValidNegSignTransform() {
+    sign = -1;
+    testTransform = new JuliaTransform(testComplex,sign);
+    Complex testPointZ = new Complex(2, 6);
+
+    Complex result = (Complex) testTransform.transform(testPointZ);
+
+    assertEquals(-5, result.getRealPart());
+    assertEquals(-4, result.getImaginaryPart());
+  }
+  
+  /**
+   * Negative test:
+   * Test Julia transform with null point.
+   */
+  @Test
+  public void testNullPointTransform() {
+    sign = 1;
+    testTransform = new JuliaTransform(testComplex,sign);
+    Complex testPointZ = null;
+    
+    assertThrows(NullPointerException.class, () -> {
+      testTransform.transform(testPointZ);
+    });
+  }
+}
