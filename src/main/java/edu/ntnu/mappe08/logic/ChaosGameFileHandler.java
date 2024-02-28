38432e79306315317d70308ba61382dcb3b4ea25
matrHS <matthew.r.hunt@gmail.com>
2024-02-28 09:58:53 +0100

    Started work on FileHandler

---
+++ src/main/java/edu/ntnu/mappe08/logic/ChaosGameFileHandler.java
@@ -0,0 +1,14 @@
+package edu.ntnu.mappe08.logic;
+
+public class ChaosGameFileHandler {
+  
+  public ChaosGameFileHandler() {
+  }
+  
+  public ChaosGameDescription readFromFile(String path) {
+    return null;
+  }
+  
+  public void writeToFile(ChaosGameDescription description, String path) {
+  }
+}
