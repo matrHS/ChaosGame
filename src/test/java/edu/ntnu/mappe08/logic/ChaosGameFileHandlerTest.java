e063af2b24155afb5cbf42832e166b28c1556a17
matrHS <matthew.r.hunt@gmail.com>
2024-02-28 11:49:57 +0100

    started creating tests for file handler

---
+++ src/test/java/edu/ntnu/mappe08/logic/ChaosGameFileHandlerTest.java
@@ -0,0 +1,18 @@
+package edu.ntnu.mappe08.logic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class ChaosGameFileHandlerTest {
+  
+  String testAffinePath = "";
+  String testJuliaPath = "";
+  
+  @BeforeEach
+  void initializeTestValues() {
+    testAffinePath = "testAffine.csv";
+    testJuliaPath = "testJulia.csv";
+  }
+  
+}
